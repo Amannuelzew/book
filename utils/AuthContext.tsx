8281ddc560a869d1d91772b537de5a52342495e1
amanzew <amanzew@gmail.com>
2024-08-12 18:36:52 +0300

    remove authcontext

--- utils/AuthContext.tsx
+++ utils/AuthContext.tsx
@@ -1,23 +1,23 @@
-"use client";
-import { createContext, useState, useEffect } from "react";
-import { defineAbilityFor } from "@/utils/ability";
+// "use client";
+// import { createContext, useState, useEffect } from "react";
+// import { defineAbilityFor } from "@/utils/ability";
 
-const AuthContext = createContext({});
+// const AuthContext = createContext({});
 
-export function AuthProvider({ children }) {
-  const [user, setUser] = useState();
-  const [ability, setAbility] = useState();
+// export function AuthProvider({ children }) {
+//   const [user, setUser] = useState();
+//   const [ability, setAbility] = useState();
 
-  useEffect(() => {
-    // Replace with your actual user fetching logic
-    const fetchedUser = { id: 1, role: "admin" }; // Example user
-    setUser(fetchedUser);
-    setAbility(defineAbilityFor(fetchedUser));
-  }, []);
+//   useEffect(() => {
+//     // Replace with your actual user fetching logic
+//     const fetchedUser = { id: 1, role: "admin" }; // Example user
+//     setUser(fetchedUser);
+//     setAbility(defineAbilityFor(fetchedUser));
+//   }, []);
 
-  return (
-    <AuthContext.Provider value={{ user, ability }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
+//   return (
+//     <AuthContext.Provider value={{ user, ability }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// }
